bb12c34c1a941b56612f4910779c3c70e8ede8f2
diegoberti <diegiberti@gmail.com>
2024-11-27 10:20:32 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,7 +24,7 @@ def alg_vinc(f, g, x0=0,y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', 
     
     #im = ax.imshow(Z, extent=(x0-d, x0+d, y0-d, y0+d), norm=colors.SymLogNorm(linthresh=lnrwidth, linscale=1,
                                              # vmin=Z.min(), vmax=Z.max(), base=10))
-   # im = ax.imshow(Z, extent=(x0-d, x0+d, y0-d, y0+d), vmin=Z.min(), vmax=Z.max())
+   # im = ax.imshow(Z, extent=e(x0-d, x0+d, y0-d, y0+d), vmin=Z.min(), vmax=Z.max())
     im2 = ax.pcolormesh(X,Y,Z, vmin=Z.min(), vmax = Z.max(), cmap=col)
     im = ax.pcolormesh(X,Y,Z, norm=colors.SymLogNorm(linthresh=0.5, linscale=1, vmin=Z.min(), vmax=Z.max(), base = 10), cmap=col)
     fig.colorbar(im2, extend='both', orientation='horizontal', shrink=0.8)
@@ -44,7 +44,7 @@ def alg_vinc(f, g, x0=0,y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', 
 
     return fig 
 
-def alg(f, x0=0, y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', level=0, Blevel=False):
+def alg(f, x0=0, y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', level=0, Blevel=False, dplot =False):
     x = np.arange(x0 - d, x0 + d, 2 * d / 500)
     y = np.arange(y0 - d, y0 + d, 2 * d / 500)
     X, Y = np.meshgrid(x, y)
@@ -63,7 +63,7 @@ def alg(f, x0=0, y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', level=0
     f0 = f(x0,y0)
     #levels = [f0 + e * k for k in np.arange(-15, 15)]
     CS1 = ax2.contour(X, Y, Z, [f0 + 2 * e * k for k in np.arange(1, 16)], linewidths=1.5, cmap='Reds')
-    ax2.contour(X,Y,Z, [f0], lw=1.5, colors='black')
+    ax2.contour(X,Y,Z, [f0], linewidths=1.5, colors='black')
     CS2 = ax2.contour(X, Y, Z, [f0 + 2 * e * k for k in np.arange(-15, 0)], linewidths=1.5, cmap='Blues_r')
 
     #ax2.clabel(CS1)
@@ -85,7 +85,30 @@ def alg(f, x0=0, y0=0, d=1, e=0.01, cl=True, center=True, col='viridis', level=0
     ax1.set_aspect('equal')
     ax2.set_aspect('equal')
 
-    return fig1, fig2
+    if dplot:
+
+        #fig3, ax3 = plt.figure().add_subplot(projection='3d')
+        fig3 = plt.figure(figsize=(10, 7)) 
+        ax3 = fig3.add_subplot(111, projection='3d')
+
+        vmin = Z.min()
+        vmax = Z.max()
+        c_range = [0.05 * k * vmax for k in np.arange(0, 10)]
+        c_range2 = [vmin + 0.1 * k * abs(vmin) for k in np.arange(0, 10)]
+
+        if vmax > 0:
+            ax3.contour(X, Y, Z, c_range, cmap='Reds', linewidths=1.5)
+
+        if vmin < 0:
+            ax3.contour(X, Y, Z, c_range2, cmap='Blues_r', linewidths=1.5)
+
+        #ax2.contour(X,Y,Z, [0], linewidths=1.5)
+        ax3.plot_surface(X, Y, Z, cmap="coolwarm", rstride=1, cstride=1, alpha=0.2)
+    
+    if dplot == False:
+        fig3 = fig2
+
+    return fig1, fig2, fig3
 
 # Streamlit interface
 st.title("Generatore di grafico dei livelli in 2D con input simbolico")
@@ -158,6 +181,9 @@ else:
     livello_f=0
     #st.write("Seleziona la checkbox per inserire il livello che vuoi visualizzare")
 
+dplot_f = st.checkbox(r"Scegli se visualizzare il grafico in 3D", value=False)
+
+
 
 
 # When the user clicks the button, generate the plot
@@ -170,9 +196,14 @@ if st.button("Genera i grafici"):
 
         # Generate and display the contour plot
         if vincolo == False:
-            fig1, fig2 = alg(f, x0, y0, lato, passo_attorno_f_0, center=center, col=colormap, level=livello_f, Blevel=curva_livello_f)
-            st.pyplot(fig1)
-            st.pyplot(fig2)
+            fig1, fig2, fig3 = alg(f, x0, y0, lato, passo_attorno_f_0, center=center, col=colormap, level=livello_f, Blevel=curva_livello_f, dplot = dplot_f)
+            if dplot_f == False:
+                st.pyplot(fig1)
+                st.pyplot(fig2)
+            if dplot_f == True:
+                st.pyplot(fig1)
+                st.pyplot(fig2)
+                st.pyplot(fig3)
         if vincolo:
             fig = alg_vinc(f, g, x0, y0, lato, passo_attorno_f_0, center=center, col=colormap, level=livello_f, Blevel=curva_livello_f)
             st.pyplot(fig)
