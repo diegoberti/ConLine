d046e948448c8cab48ef15432bfc23aeb79699eb
diegoberti <diegiberti@gmail.com>
2025-11-07 17:09:47 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -152,7 +152,7 @@ st.title("Esplora le curve di livello")
 
 # Collect user input
 st.markdown(
-    r"<span style='color:#1f4e79'>Scegli il quadrato $Q$ centrato in $(x_0, y_0)$ e di lato $2\ell$</span>",
+    r"<span style='color:#1f4e79'>$\bullet$ Scegli il quadrato $Q$ centrato in $(x_0, y_0)$ e di lato $2\ell$</span>",
     unsafe_allow_html=True
 )
 #st.write(r"Scegli il quadrato $Q$, di centro $(x_0,y_0)$ e lato $\ell$, dove visualizzare i livelli di $f$", )
